61d960bfa41bb8d587e87a13ef1d52ec5a830305
Your Name <you@example.com>
2026-07-10 07:13:57 -0400

    Auto-deploy: Fri 07/10/2026  7:13:55.63

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,6 +2,7 @@ import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
+// Simple, clean initialization
 export const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
