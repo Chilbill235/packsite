746194635ed89b59a5ec98f1803bf056d7ca5dd5
Your Name <you@example.com>
2026-07-10 07:29:27 -0400

    Auto-deploy: Fri 07/10/2026  7:29:26.73

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,8 +2,16 @@ import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-// Simple, clean initialization
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    // Providing a valid object with 'datasources' satisfies the validation requirement
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
+  });
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
